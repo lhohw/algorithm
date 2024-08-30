a4e84cf70977081de9e13a91a9076c889935b974
lhohw <lho.hw.an1@gmail.com>
2024-08-30 14:10:17 +0900

    24.08.30

---
+++ tree/BOJ15900/index.ts
@@ -0,0 +1,65 @@
+import { readFileSync } from "fs";
+
+export const parseInput = (input: string) => {
+  const splitted = input.split("\n");
+  const n = +splitted.shift()!;
+  const edges = splitted.map((row) => row.split(" ").map((e) => +e - 1));
+
+  return { n, edges };
+};
+
+export const solve = (n: number, edges: number[][]) => {
+  const graph = createGraph(n, edges);
+  const total = bfs(n, graph);
+  return serialize(total);
+};
+
+export const createGraph = (n: number, edges: number[][]) => {
+  const graph: number[][] = Array.from({ length: n }).map(() => []);
+
+  for (const [a, b] of edges) {
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  return graph;
+};
+
+export const bfs = (n: number, graph: number[][]) => {
+  const queue: number[] = [];
+  let queueIdx = 0;
+  const depth = new Array(n).fill(0);
+  let totalDepth = 0;
+
+  queue.push(0);
+  depth[0] = 1;
+
+  while (queueIdx !== queue.length) {
+    const here = queue[queueIdx++];
+    let isLeaf = true;
+
+    for (const there of graph[here]) {
+      if (depth[there]) continue;
+
+      isLeaf = false;
+      depth[there] = depth[here] + 1;
+      queue.push(there);
+    }
+
+    if (isLeaf) totalDepth += depth[here] - 1;
+  }
+
+  return totalDepth;
+};
+
+export const serialize = (total: number) => (total % 2 === 0 ? "No" : "Yes");
+
+const print = () => {
+  const input = readFileSync("/dev/stdin").toString().trim();
+  const { n, edges } = parseInput(input);
+  const ret = solve(n, edges);
+
+  console.log(ret);
+};
+
+print();
